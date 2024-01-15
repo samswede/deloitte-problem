d15700ddd95ba2807b980f1761236dea7a3e0bf3
Samuel Andersson <58047562+samswede@users.noreply.github.com>
2024-01-15 20:26:49 +0100

    wertzu

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from transformers import AutoProcessor, AutoModelForSpeechSeq2Seq
 from transcription import transcribe_audio
 
-# Load the processor and model
-processor = AutoProcessor.from_pretrained("openai/whisper-tiny.en") # whisper-tiny.en  # openai/whisper-large-v3
-model = AutoModelForSpeechSeq2Seq.from_pretrained("openai/whisper-tiny.en") # whisper-tiny.en  # openai/whisper-large-v3
+def main():
+    # Load the processor and model
+    processor = AutoProcessor.from_pretrained("openai/whisper-tiny.en") 
+    model = AutoModelForSpeechSeq2Seq.from_pretrained("openai/whisper-tiny.en")
 
-# Specify the path to your audio file
-audio_path = "audio/EarningsCall.wav"
+    # Specify the path to your audio file
+    audio_path = "audio/EarningsCall.wav"
 
-# Call the function to transcribe the audio
-predicted_text = transcribe_audio(audio_path, processor, model)
+    # Call the function to transcribe the audio
+    predicted_text = transcribe_audio(audio_path, processor, model)
 
-# Print or use the predicted text
-print(predicted_text)
+    # Print or use the predicted text
+    print(predicted_text)
+
+if __name__ == "__main__":
+    main()
--- transcription.py
+++ transcription.py
@@ -1,20 +1,32 @@
-from transformers import AutoProcessor, AutoModelForSpeechSeq2Seq
 import torchaudio
 import torch
 
 def transcribe_audio(audio_path, processor, model):
-    # Load the audio file using torchaudio
-    waveform, sample_rate = torchaudio.load(audio_path)
+    try:
+        # Load the audio file using torchaudio
+        waveform, sample_rate = torchaudio.load(    audio_path, 
+                                                    normalize=True)
 
-    # Preprocess the audio using the processor
-    inputs = processor(waveform, return_tensors="pt")
+        print(f"waveform shape: {waveform.shape}")
+        print(f"Sample rate: {sample_rate}")
 
-    # Perform inference
-    with torch.no_grad():
-        outputs = model(input_values=inputs.input_values)
+        # Preprocess the audio using the processor
+        inputs = processor( waveform.squeeze().numpy(), 
+                            return_tensors="pt", 
+                            sample_rate=sample_rate)
 
-    # Extract the predicted text from the model's output
-    predicted_ids = outputs.logits.argmax(-1)
-    predicted_text = processor.batch_decode(predicted_ids)
+        # Perform inference
+        with torch.no_grad():
+           outputs = model(input_values=inputs.input_values)
+        
+        # Extract the predicted text from the model's output
+        predicted_ids = outputs.logits.argmax(-1)
+        predicted_text = processor.batch_decode(predicted_ids)
 
-    return predicted_text
+        return predicted_text
+    
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+        return None
+
